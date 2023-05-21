cf7ed98a01c7eabfd9fa3882514f75e3b6116f5d
zjq <526914817@qq.com>
2023-05-21 20:34:51 +0800

    qew

---
+++ src/main/java/com/example/asdfdsad/AsdfdsadApplication.java
@@ -0,0 +1,13 @@
+package com.example.asdfdsad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsdfdsadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsdfdsadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/asdfdsad/AsdfdsadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.asdfdsad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsdfdsadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
